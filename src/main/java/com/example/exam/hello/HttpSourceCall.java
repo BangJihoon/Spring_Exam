44854d2fbccc82dd59bec3af362b4cb5cf61fbd9
Bangjihoon <jihoon289@naver.com>
2022-03-11 21:49:29 +0900

    init

---
+++ src/main/java/com/example/exam/hello/HttpSourceCall.java
@@ -0,0 +1,11 @@
+package com.example.exam.hello;
+
+public class HttpSourceCall {
+    private final HttpSourceCall instance;
+
+    public HttpSourceCall(HttpSourceCall instance) {
+        this.instance = instance;
+    }
+
+
+}
