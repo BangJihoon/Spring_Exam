0f28c29e5512f8c32239353b9d5bdd18fa13824a
Bangjihoon <jihoon289@naver.com>
2022-03-13 02:45:09 +0900

    Feat - 몫과 나머지 정상 생성 및 테스트

--- src/main/java/com/example/exam/parsing/ParsingHtml.java
+++ src/main/java/com/example/exam/parsing/ParsingHtml.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class ParsingHtml {
@@ -34,7 +35,8 @@ public class ParsingHtml {
         return contents.toString();
     }
     // 파싱하기
-    public String result(String html, int type){
+    public String[] result(String html, int type, int bundle){
+        // Type별 정규표현식을 지정하여 파싱
         String pattern="";
         if(type ==1){
             pattern = "<[^>]*>"; // 태그내용 제거
@@ -43,7 +45,36 @@ public class ParsingHtml {
         }
         html = html.replaceAll(pattern,"");
 
-        return html;
-    }
+        // 숫자부분과 알파벳 부분 저장
+        String htmlNumber = html.replaceAll("[^0-9]","");
+        String htmlAlpha= html.replaceAll("[^a-zA-Z]","");
 
+        // 오름차순 정렬
+        htmlNumber = SortStirng(htmlNumber);
+        htmlAlpha = SortStirng(htmlAlpha);
+
+        // 교차병합
+        int min = Math.min(htmlAlpha.length(), htmlNumber.length());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < min; i++) {
+             sb.append(htmlAlpha.charAt(i)).append(htmlNumber.charAt(i));
+        }
+        // 나머지 연결 - 둘중 남은거만 연결될꺼임
+        sb.append(htmlAlpha.substring(min))
+                .append(htmlNumber.substring(min));
+        // bundle로 몫과 나머지 계산
+        int share = sb.length() / bundle;
+        String shareStr = sb.substring(0,share*bundle);
+        String remainStr = sb.substring(share*bundle);
+
+        String[] res = new String[2];
+        res[0] = shareStr;
+        res[1] = remainStr;
+        return res;
+    }
+    public static String SortStirng(String s){
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        return String.valueOf(arr);
+    }
 }
--- src/test/java/com/example/exam/parsing/ParsingHtmlTest.java
+++ src/test/java/com/example/exam/parsing/ParsingHtmlTest.java
@@ -14,8 +14,9 @@ class ParsingHtmlTest {
     public void  resultTest(){
         ParsingHtml parsingHtml = new ParsingHtml();
         String html = parsingHtml.getHtml("https://velog.io/@outstandingboy/Git-%EC%BB%A4%EB%B0%8B-%EB%A9%94%EC%8B%9C%EC%A7%80-%EA%B7%9C%EC%95%BD-%EC%A0%95%EB%A6%AC-the-AngularJS-commit-conventions");
-        String result = parsingHtml.result(html, 1);
-        System.out.println(result);
+        String[] result = parsingHtml.result(html, 1,3);
+        System.out.println("result[0] = " + result[0]);
+        System.out.println("result[1] = " + result[1]);
     }
 
 }
